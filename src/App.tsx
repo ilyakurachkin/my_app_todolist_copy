56dce02b51933901384396316ccf2513cd8c7d2c
Ilya Kurachkin <patlatov@inbox.ru>
2022-05-24 22:13:47 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import {Todolist} from "./Todolist";
+
+
+function App() {
+
+    const tasks1 = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "ReactJS", isDone: false }
+    ]
+    const tasks2 = [
+        { id: 1, title: "Hello world", isDone: true },
+        { id: 2, title: "I am Happy", isDone: false },
+        { id: 3, title: "Yo", isDone: false }
+    ]
+
+    return (
+        <div className="App">
+            <Todolist title={'what to learn1'} tasks={tasks1}/>
+            <Todolist title={'what to learn2'} tasks={tasks2}/>
+        </div>
+    );
+}
+
+export default App;
